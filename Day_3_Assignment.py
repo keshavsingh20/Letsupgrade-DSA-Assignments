871e6418aa5fe806a618c9fd0dfae8a10688a755
KESHAV SINGH YADAV <65011436+keshavsingh20@users.noreply.github.com>
2021-01-21 20:52:40 +0530

    Day 3 assignment submitted

---
+++ Day_3_Assignment.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Question 1:
+# Write a Python program to add two numbers using class and object.
+# (Take both numbers as input from the user)
+
+# In[2]:
+
+
+class Add:
+
+    def findsum(self, a, b):
+        s = a + b
+        return s
+
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+obj = Add()
+s = obj.findsum(a, b)
+
+print("Sum is:", s)
+
+
+# # Question 2:
+# Define a function swap that should swap two values and print the swapped variables outside the
+# swap function.
+
+# In[15]:
+
+
+def swap(a,b):
+    a,b=b,a
+    print("After swap a is:",a)
+    print("After swap b is:",b)
+
+
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+swap(a,b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
